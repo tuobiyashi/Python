8f18fbfe77cfe70b6bdef28fe9cf6200700158b9
tuobiyashi <helong@usa.com>
2018-12-02 23:00:39 +0800

    password

--- day01/passwd.py
+++ day01/passwd.py
@@ -1,4 +1,14 @@
+import getpass
+
+_username = "helong"
+_password = "genius"
 username = input("username:")
 password = input("password:")
+# password1 = getpass.getpass("password1:")
 
-print(username, password)
+if _username == username and _password == password:
+    print("welcom user {name} login...".format(name=username))
+else:
+    print("Invalid username or password...")
+
+# print(username, password)
